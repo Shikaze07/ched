e097f6af16e7e959bb3d11dee827b89990beb2f0
Shikaze07 <marcosmontano2003@gmail.com>
2026-02-22 05:06:36 +0800

    sonner top-right position

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         <Providers>
           <NavHeader />
           {children}
-          <Toaster position="top-center" richColors />
+          <Toaster position="top-right" richColors />
         </Providers>
 
       </body>
